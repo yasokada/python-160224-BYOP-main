7f63741ec50268fcc9833f848b70d876f6839107
yasokada <yasokada@gmail.com>
2016-02-24 21:24:35 +0900

    add serial print

--- BYOP-main.py
+++ BYOP-main.py
@@ -1,3 +1,32 @@
 #!/usr/bin/env python
 
-print "hello"
+#--- selection of import based on the package ---
+''' 1. with python3-serial'''
+import serial
+''' 2. without python3-serial'''
+#from dummySerial import CDummySerial
+#serial = CDummySerial()
+#-----------------
+
+'''
+v0.2 2016 Feb. 24
+  - add serial print
+v0.1 2016 Feb. 24
+  - just hello
+'''
+
+import time
+usleep = lambda x: time.sleep(x/1000000.0)
+
+con1 = serial.Serial('/dev/ttyAMA0', 115200, timeout=0.1)
+
+def main():
+    while True:
+        time.sleep(1.0); # second
+        msg = "hello,1,7of9\n"
+        print msg,
+        con1.write(msg)
+
+if __name__ == '__main__':
+    main()
+    con1.close()   
