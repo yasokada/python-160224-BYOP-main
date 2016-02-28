bcf744f455fae54392cd84377a9f76ec2ba3a921
yasokada <yasokada@gmail.com>
2016-02-29 06:56:17 +0900

    add debug_outputDebugString()

--- BYOP-main.py
+++ BYOP-main.py
@@ -9,6 +9,8 @@ import serial
 #-----------------
 
 '''
+v0.5 2016 Feb. 29
+  - add debug_outputDebugString()
 v0.4 2016 Feb. 24
   - add contact by Vital
 v0.3 2016 Feb. 24
@@ -24,6 +26,7 @@ v0.1 2016 Feb. 24
 '''
 
 import time
+import os.path
 usleep = lambda x: time.sleep(x/1000000.0)
 
 con1 = serial.Serial('/dev/ttyAMA0', 115200, timeout=0.1)
@@ -39,7 +42,15 @@ cmdlines = [
     "post,7of9,hello_7of9,0"
     ]
 
-def main():    
+def debug_outputDebugString(prfx, msg):
+    print "[DEBUG]" + prfx + "," + msg
+
+def read_name():
+    debug_outputDebugString("read_name","Line49 > start")
+    
+def main():
+    read_name()
+    
     for idx in range (8):
         msg = cmdlines[idx] + "\n"
         print msg,
